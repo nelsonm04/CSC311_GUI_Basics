c47871aec0eb2a79c1650bb2bd31f41ee70ecb95
Nelson Mojica <nelsonm43@icloud.com>
2025-02-12 13:27:48 -0500

    new button that rotates on click

--- src/main/java/com/example/csc311_gui_basics/GUIController.java
+++ src/main/java/com/example/csc311_gui_basics/GUIController.java
@@ -1,6 +1,8 @@
 package com.example.csc311_gui_basics;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -19,15 +21,27 @@ public class GUIController {
     private ImageView bunnyPic;
 
     @FXML
+   private Button newButton;
+    int x =10;
+
+
+    @FXML
     protected void onHelloButtonClick() {
         String name= yourName.getText();
         welcomeText.setText("Welcome " + name );
-        nelson.rotateProperty().setValue(180);
+        nelson.setRotate(45);
+
     }
     @FXML
     public void initialize() {
         Image image = new Image(getClass().getResource("/com/example/csc311_gui_basics/bunny.jpeg").toExternalForm());
         bunnyPic.setImage(image);
+    }
+
+    @FXML
+    void rotateMe(ActionEvent event) {
+        newButton.setRotate(x++);
+
     }
 
 }
